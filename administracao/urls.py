e12cddf76e09419165c57e95834972054feab317
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-07-09 10:54:58 -0300

    Listagem de usuários finalizada

--- administracao/urls.py
+++ administracao/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('servicos/listar', servico_views.listar_servicos, name='listar_servicos'),
     path('servicos/editar/<int:id>', servico_views.editar_servico, name='editar_servico'),
     path('usuarios/cadastrar', usuario_views.cadastrar_usuario, name='cadastrar_usuario'),
+    path('usuarios/listar', usuario_views.listar_usuarios, name='listar_usuarios'),
 ]
--- administracao/views/usuario_views.py
+++ administracao/views/usuario_views.py
@@ -1,11 +1,18 @@
 from ..forms.usuario_forms import UsuarioForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
 
 def cadastrar_usuario(request):
     if request.method == "POST":
         form_usuario = UsuarioForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
+            return redirect('listar_usuarios')
     else:
         form_usuario = UsuarioForm()
     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+def listar_usuarios(request):
+    User = get_user_model()
+    usuarios = User.objects.filter(is_superuser=True)
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
