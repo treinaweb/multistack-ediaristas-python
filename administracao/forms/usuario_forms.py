f52f8360e703d2fe35939b8ebf14b3b97d2cff35
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-07-12 09:04:33 -0300

    Alterando campos do cadastro de usuario

--- administracao/forms/usuario_forms.py
+++ administracao/forms/usuario_forms.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import get_user_model
 
 class UsuarioForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email', 'password1', 'password2']
+
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.is_superuser = True
