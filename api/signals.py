5fad317c71a9b6c98c901e5002a4572b5b98c671
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-08-16 20:39:22 -0300

    05.03_Criando_Estrutura_Email_Django

--- api/signals.py
+++ api/signals.py
@@ -1,9 +1,16 @@
 from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .models import Usuario
 
 def usuario_cadastrado(sender, instance, created, **kwargs):
-    print("Usuário cadastrado")
-    print(instance.nome_completo)
+    if created:
+        assunto = 'Cadastro realizado com sucesso'
+        corpo_email = ''
+        email_destino = [instance.email, ]
+        email_remetente = 'fagner.pinheiro@treinaweb.com.br'
+        mensagem_html = render_to_string('email_cadastro.html', {'usuario': instance})
+        send_mail(assunto, corpo_email, email_remetente, email_destino, mensagem_html)
 
 post_save.connect(usuario_cadastrado, sender=Usuario)
 
