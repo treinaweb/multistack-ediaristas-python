8720328bd811f0c91a80e47fdd2125409a08dde6
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-08-18 14:27:32 -0300

    03.01_Criando_Model_Diaria

--- api/models.py
+++ api/models.py
@@ -4,9 +4,11 @@ import os
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import validate_image_file_extension
+from django.db.models.expressions import F
 from localflavor.br.models import BRCPFField
 from .managers import diarista_manager
 from django.contrib.auth.models import UserManager
+from administracao.models import Servico
 
 
 # Create your models here.
@@ -49,6 +51,50 @@ class Usuario(AbstractUser):
 
     objects = UserManager()
     diarista_objects = diarista_manager.DiaristaManager()
+
+
+class Diaria(models.Model):
+    STATUS_DIARIA_CHOICES = (
+        (1, "SEM_PAGAMENTO"),
+        (2, "PAGO"),
+        (3, "CONFIRMADO"),
+        (4, "CONCLUIDO"),
+        (5, "CANCELADO"),
+        (6, "AVALIADO"),
+        (7, "TRANSFERIDO")
+    )
+
+    data_atendimento = models.DateField(null=False, blank=False)
+    tempo_atendimento = models.IntegerField(null=False, blank=False)
+    status = models.IntegerField(null=False, blank=False, choices=STATUS_DIARIA_CHOICES,
+    default=1)
+    preco = models.FloatField(null=False, blank=False)
+    valor_comissao = models.FloatField(null=False, blank=False)
+    logradouro = models.CharField(max_length=60, null=False, blank=False)
+    numero = models.CharField(max_length=10, null=False, blank=False)
+    bairro = models.CharField(max_length=30, null=False, blank=False)
+    complemento = models.CharField(max_length=100, null=False, blank=False)
+    cep = models.CharField(max_length=10, null=False, blank=False)
+    cidade = models.CharField(max_length=30, null=False, blank=False)
+    estado = models.CharField(max_length=2, null=False, blank=False)
+    codigo_ibge = models.IntegerField(null=True, blank=True)
+    quantidade_quartos = models.IntegerField(null=False, blank=False)
+    quantidade_salas = models.IntegerField(null=False, blank=False)
+    quantidade_cozinhas = models.IntegerField(null=False, blank=False)
+    quantidade_banheiros = models.IntegerField(null=False, blank=False)
+    quantidade_quintais = models.IntegerField(null=False, blank=False)
+    quantidade_outros = models.IntegerField(null=False, blank=False)
+    observacoes = models.TextField(null=False, blank=True)
+    motivo_cancelamento = models.TextField(null=True, blank=True)
+    cliente = models.ForeignKey(Usuario, null=False, blank=False, 
+    on_delete=models.DO_NOTHING, related_name='cliente')
+    diarista = models.ForeignKey(Usuario, null=True, blank=True, 
+    on_delete=models.DO_NOTHING, related_name='diarista')
+    servico = models.ForeignKey(Servico, null=False, blank=False, 
+    on_delete=models.DO_NOTHING)
+    candidatos = models.ManyToManyField(Usuario, blank=True, related_name='candidatos')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
 
 
