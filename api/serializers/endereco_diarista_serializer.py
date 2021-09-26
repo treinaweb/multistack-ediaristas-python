83e7f7260f85adf0ffab57404b71e208e5633c58
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-09-26 17:21:56 -0300

    02.04_Sobrescrevendo_Metodo_Criacao_Endereco

--- api/serializers/endereco_diarista_serializer.py
+++ api/serializers/endereco_diarista_serializer.py
@@ -5,3 +5,10 @@ class EnderecoDiaristaSerializer(serializers.ModelSerializer):
     class Meta:
         model = EnderecoDiarista
         exclude = ['usuario', ]
+
+    def create(self, validated_data):
+        usuario = self.context['request'].user
+        endereco_diarista = EnderecoDiarista.objects.update_or_create(
+         usuario=usuario, defaults=validated_data
+        )
+        return endereco_diarista
