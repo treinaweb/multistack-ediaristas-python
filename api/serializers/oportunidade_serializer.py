00ed82763f074051bf4387070f349bbf25d96f16
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-10-02 18:13:43 -0300

    02.05_Melhorando_Serilaizer_Oportunidades

--- api/serializers/oportunidade_serializer.py
+++ api/serializers/oportunidade_serializer.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from ..models import Diaria, Usuario
 
 
+class ClienteOportunidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ('nome_completo', 'telefone', 'nascimento', 'tipo_usuario', 'reputacao', 
+        'foto_usuario')
+
 class OportunidadeSerializer(serializers.ModelSerializer):
     nome_servico = serializers.SerializerMethodField()
+    cliente = ClienteOportunidadeSerializer()
     class Meta:
         model = Diaria
         fields = '__all__'
