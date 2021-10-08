6d6b4777027cce0450974103a92eba86bfde7d84
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-10-08 19:40:27 -0300

    03.04_Testando_Requisicao

--- api/services/candidatar_diarista_diaria_service.py
+++ api/services/candidatar_diarista_diaria_service.py
@@ -17,9 +17,11 @@ def relacionar_candidata_diaria(diaria_id, diarista_id):
 
 
 def contratar_diarista_diaria(diaria, diarista_id):
+    if diaria.status != 2:
+        raise serializers.ValidationError("Apenas diárias pagas podem ser realizadas")
     diarista = Usuario.objects.get(id=diarista_id)
     diaria.diarista = diarista
-    diarista.status = 3
+    diaria.status = 3
     diaria.save()
 
 def verificar_diferenca_data_contratacao(data_diaria):
--- api/views/candidatar_diarista_diaria_view.py
+++ api/views/candidatar_diarista_diaria_view.py
@@ -1,14 +1,16 @@
 from rest_framework.views import APIView
-from permissions.diarista_permission import DiaristaPermission
+from ..permissions.diarista_permission import DiaristaPermission
 from rest_framework.response import Response
 from rest_framework import status as status_http
+from ..services import candidatar_diarista_diaria_service
 
 
 class CandidatarDiaristaDiaria(APIView):
     permission_classes = [DiaristaPermission, ]
 
     def post(self, request, diaria_id, format=None):
-        # metodo para relacionar o diarista com a diaria
+        candidatar_diarista_diaria_service.relacionar_candidata_diaria(diaria_id, 
+        request.user.id)
         return Response({"Candidatura realizada com sucesso"}, 
         status=status_http.HTTP_201_CREATED)
 
