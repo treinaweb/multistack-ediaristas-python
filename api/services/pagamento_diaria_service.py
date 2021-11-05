5a8b7e9741299447bad8d4c74644543a4376cd6c
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-11-05 17:44:36 -0300

    03.01_Criando_Parametros_Pagamento

--- api/services/pagamento_diaria_service.py
+++ api/services/pagamento_diaria_service.py
@@ -1,7 +1,43 @@
+import pagarme
+import environ
 from ..models import Pagamento
-from .diaria_service import atualizar_status_diaria
+from .diaria_service import atualizar_status_diaria, listar_diaria_id
+
+env = environ.Env()
+
+env.read_env(env.str('ENV_PATH', './ediaristas/.env'))
+
+pagarme.authentication_key(env('PAGARME_KEY'))
 
 def realizar_pagamento(diaria, card_hash):
+    diaria = listar_diaria_id(diaria.id)
+    cliente = diaria.cliente
+    params = {
+        "amount": diaria.preco * 100,
+        "card_hash": card_hash,
+        "customer": {
+            "external_id": cliente.id,
+            "name": cliente.nome_completo,
+            "type": "individual",
+            "country": "br",
+            "email": cliente.email,
+            "documents": [
+                {
+                    "type": "cpf",
+                    "number": cliente.cpf
+                }
+            ],
+        },
+        "items": [
+            {
+                "id": diaria.id,
+                "title": "Diária e-diaristas",
+                "unit_price": diaria.preco,
+                "quantity": "1",
+                "tangible": False
+            }
+        ]
+    }
     Pagamento.objects.create(status="pago",
     valor=diaria.preco, transacao_id="12a9daqwe",
     diaria=diaria)
