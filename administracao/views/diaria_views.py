0211cd23310c91e284548c675edcb5fabb453157
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-11-16 18:35:15 -0300

    02.04_Filtrando_Status_Diaria

--- administracao/views/diaria_views.py
+++ administracao/views/diaria_views.py
@@ -6,7 +6,18 @@ from api.models import Diaria
 
 @login_required
 def lista_diarias(request):
-    diarias = Diaria.objects.all()
+    status = request.GET.get('status', None)
+    if status is None:
+        status_diaria = [2, ]
+    elif status == 'pendentes':
+        status_diaria = [3, ]
+    elif status == 'nao-avaliadas':
+        status_diaria = [4, ]
+    elif status == 'canceladas':
+        status_diaria = [5, 1]
+    else:
+        status_diaria = [6, 7]
+    diarias = Diaria.objects.filter(status__in=status_diaria)
     return render(request, 'diarias/lista_diarias.html', {'diarias': diarias})
 
 
