c7f106c40fdfffc1166157ce10eb151f8273d7e7
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-11-23 15:18:08 -0300

    03.02_Listando_Endereco_Diarista

--- api/views/endereco_diarista_view.py
+++ api/views/endereco_diarista_view.py
@@ -2,12 +2,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status as status_http
 from ..serializers.endereco_diarista_serializer import EnderecoDiaristaSerializer
-from ..models import EnderecoDiarista
+from ..models import EnderecoDiarista as EnderecoDiaristaModel
 from ..permissions import diarista_permission
 
 class EnderecoDiarista(APIView):
     permission_classes = [diarista_permission.DiaristaPermission, ]
     
+    def get(self, request, format=None):
+        endereco = EnderecoDiaristaModel.objects.get(usuario=request.user.id)
+        serializer_endereco_diarista = EnderecoDiaristaSerializer(endereco, 
+        context={'request': request})
+        return Response(serializer_endereco_diarista.data, status=status_http.HTTP_200_OK)
+
     def put(self, request, format=None):
         serializer_endereco_diarista = \
         EnderecoDiaristaSerializer(data=request.data, context={'request': request})
