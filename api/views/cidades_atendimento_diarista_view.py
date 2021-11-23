04a65ce88c1f171e4dbdb1653af551808db37481
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-11-23 15:32:58 -0300

    03.03_Listando_Cidades_Atendidas_Usuario

--- api/views/cidades_atendimento_diarista_view.py
+++ api/views/cidades_atendimento_diarista_view.py
@@ -5,9 +5,17 @@ from ..serializers import (cidades_atendimento_diarista_serializer,
 relacionar_cidade_diarista_serializer)
 from ..services import usuario_service, cidades_atendimento_service
 from ..permissions import diarista_permission
+from ..models import CidadesAtendimento
 
 class CidadesAtendimentoDiaristaID(APIView):
     permission_classes = [diarista_permission.DiaristaPermission, ]
+
+    def get(self, request, format=None):
+        cidades_usuario = CidadesAtendimento.objects.filter(usuario=request.user.id)
+        serializer_cidades_atentimento = cidades_atendimento_diarista_serializer.\
+                    CidadesAtendimentoDiaristaSerializer(cidades_usuario, many=True)
+        return Response(serializer_cidades_atentimento.data)
+
     def put(self, request, format=None):
         serializer_cidades_atendimento = relacionar_cidade_diarista_serializer\
             .RelacionarCidadeDiaristaSerializer(data=request.data)
