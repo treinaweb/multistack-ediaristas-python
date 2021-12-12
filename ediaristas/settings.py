c9fa55bad181c6ae8a9d5e9ae4bd620f1aab7f6b
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-12-12 16:40:59 -0300

    add

--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ediaristas-django.herokuapp.com', ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
