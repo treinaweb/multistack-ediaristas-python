43bd8c395fa12957190564cf61fb1faad4c717a1
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2022-01-04 19:17:26 -0300

    correcoes database_url

--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -140,7 +140,7 @@ WSGI_APPLICATION = 'ediaristas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=600) }
 
 
 # Email
